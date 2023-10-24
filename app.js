0eeae38d2d85e9174a7d2cd7f8b91f1e3e6cbecc
동현 <Garry@example.com>
2023-10-24 15:59:26 +0900

    get 방식으로 req, res 처리

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/home.html') {
+    // 요청이 GET이며 URL이 '/home.html'일 때,
+    const filePath = path.join(__dirname, 'home.html');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else {
+    // 다른 요청에 대한 응답 처리
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
