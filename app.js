8649adf39ffef0ca6021e486942d9dae8029a5a5
Garry <personarpg7501@gmail.com>
2023-10-26 06:34:50 +0900

    home.html 파일을 간략화 하기 위한 분리작업 시도

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/') {
-    // 요청이 GET이며 URL이 '/home.html'일 때,
+  if (req.method === 'GET' && req.url === '/home.html') {
     const filePath = path.join(__dirname, 'home.html');
 
     fs.readFile(filePath, 'utf8', (err, data) => {
@@ -12,18 +11,39 @@ const server = http.createServer((req, res) => {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Internal Server Error');
       } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
+        // home.html을 읽은 후, style.css와 Calendar-Script.js도 읽어옵니다.
+        const styleFilePath = path.join(__dirname, 'style.css');
+        const scriptFilePath = path.join(__dirname, 'Calendar-Script.js');
+
+        fs.readFile(styleFilePath, 'utf8', (styleErr, styleData) => {
+          if (styleErr) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal Server Error');
+          } else {
+            fs.readFile(scriptFilePath, 'utf8', (scriptErr, scriptData) => {
+              if (scriptErr) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+              } else {
+                // 모든 파일을 성공적으로 읽었을 때, 응답으로 모든 파일을 함께 보냅니다.
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                // home.html 내부에 style.css와 Calendar-Script.js를 포함합니다.
+                data = data.replace('<!-- insert-css-here -->', `<style>${styleData}</style>`);
+                data = data.replace('<!-- insert-js-here -->', `<script>${scriptData}</script>`);
+                res.end(data);
+              }
+            });
+          }
+        });
       }
     });
   } else {
-    // 다른 요청에 대한 응답 처리
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
   }
 });
 
-const port = 8080; // 사용할 포트 번호를 선택하세요.
+const port = 8080;
 server.listen(port, () => {
   console.log(`서버가 ${port}번 포트에서 실행 중입니다.`);
 });
